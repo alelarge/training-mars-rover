c5a15ee72ac7159da2ad69c581546d4dcbb8fd71
alelarge <armelle.lelarge@shodo.io>
2024-06-22 20:27:42 +0200

    first commit

---
+++ src/test/kotlin/TestAcceptation.kt
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class AcceptationTest {
+  @Test
+    fun `rover should move forward`() {
+       /* val rover = MarsRover(Position(0, 0), Direction.NORTH)
+        rover.execute("F")
+        assertEquals(Position(0, 1), rover.position)
+        assertEquals(Direction.NORTH, rover.direction)*/
+
+        TODO()
+
+
+    }
+}
