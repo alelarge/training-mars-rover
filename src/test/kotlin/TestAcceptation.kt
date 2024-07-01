2bb24bc3b65045b2a8916efe7ef6262be20cb629
alelarge <armelle.lelarge@shodo.io>
2024-07-01 17:16:04 +0200

    add test acceptation failed rover should turn right

--- src/test/kotlin/TestAcceptation.kt
+++ src/test/kotlin/TestAcceptation.kt
@@ -9,4 +9,12 @@ class AcceptationTest {
         assertEquals(Position(0, 1), rover.position)
         assertEquals(Direction.NORTH, rover.direction)
     }
+
+  @Test
+  fun `rover should turn right`() {
+    /*  val rover = MarsRover(Position(0, 0), Direction.NORTH)
+      rover.execute("R")
+      assertEquals(Direction.EAST, rover.direction)*/
+      TODO()
+  }
 }
